52604fb232d115f259ce6813060577f676c05f91
Beibit <asylbekulybeibit@gmail.com>
2024-12-10 15:42:51 +0500

    Show quote and author

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 const quotes = [
-  "Сила не в том, чтобы никогда не падать, а в том, чтобы подниматься каждый раз, когда падаешь. - Конфуций",
-  "Единственный способ добиться успеха — делать то, что вы любите. - Стив Джобс",  
-    "Счастье не в том, чтобы иметь всё, а в умении видеть радость в малом. - Лев Толстой",  
+  {
+    quote: "Сила не в том, чтобы никогда не падать, а в том, чтобы подниматься каждый раз, когда падаешь",
+    author: "Конфуций",
+  },
+  {
+    quote: "Единственный способ добиться успеха — делать то, что вы любите",
+    author: "Стив Джобс",
+  },
+  {
+    quote: "Счастье не в том, чтобы иметь всё, а в умении видеть радость в малом",
+    author: "Лев Толстой",
+  },  
 ];
 
 const quoteElement = document.getElementById("quote");
+const quoteAuthorElement = document.getElementById('quote-author');
 const generateBtn = document.getElementById("generate-btn");
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomIndex];
-  quoteElement.textContent = randomQuote;  
+  const randomQuote = quotes[randomIndex];  
+  const {quote, author} = randomQuote
+  quoteElement.textContent = quote; 
+  quoteAuthorElement.textContent = author; 
+
 }
 
 generateBtn.addEventListener("click", generateRandomQuote);
